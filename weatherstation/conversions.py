261739d80c8256ca9fbaca748ad7f466638b688e
Peter Reimer <peter@4pi.org>
2020-08-17 20:12:36 +0200

    beaufort scale

--- weatherstation/conversions.py
+++ weatherstation/conversions.py
@@ -9,10 +9,6 @@ def si_conversion(params):
         "indoortempf": ["temp_indoor", f2c],
         "tempf": ["temp_outdoor", f2c],
         "dewptf": ["temp_dewpoint", f2c],
-        "windchillf": ["temp_windchill", f2c],
-        "windspeedmph":["wind_speed", m2k],
-        "windgustmph": ["wind_gust", m2k],
-        "winddir": ["wind_direction", make_float],
         "absbaromin": ["pressure_abs", i2h],
         "baromin": ["pressure_rel", i2h],
         "rainin": ["rain_now", i2m],
@@ -24,6 +20,10 @@ def si_conversion(params):
         "humidity": ["humidity_outdoor", make_float],
         "solarradiation": ["solarradiation", make_float],
         "UV": ["uv_index", make_float],
+        "windchillf": ["temp_windchill", f2c],
+        "windspeedmph":["wind_speed", m2k],
+        "windgustmph": ["wind_gust", m2k],
+        "winddir": ["wind_direction", make_float],
         }
     params_si = collections.OrderedDict()
     for key in conversions.keys():
@@ -31,6 +31,10 @@ def si_conversion(params):
         if key in params.keys():
             #print("old: %s, new: %s value: %s" % (key, new_key, params[key]))
             params_si[new_key] = conversions[key][1](params[key])
+    
+    # extra: Beauford scale and compass direction of wind
+    params_si["wind_compass"] = deg2compass(float(params["winddir"])) 
+    params_si["wind_beaufort"] = wind2beaufort(float(params["windspeedmph"])) 
     return params_si 
 
 def f2c(fahrenheit):
@@ -63,7 +67,32 @@ def utc2local(utc_string):
     local = utc.replace(tzinfo=timezone.utc).astimezone(tz=None)
     return local.isoformat()
 
+def deg2compass(degree):
+    """https://stackoverflow.com/questions/7490660/converting-wind-direction-in-angles-to-text-words/7490772#7490772"""
+    val = int(( degree / 22.5 ) + 0.5 )
+    arr = ["N","NNO","NO","ONO","O","OSO", "SO", "SSO","S","SSW","SW","WSW","W","WNW","NW","NNW"]
+    return arr[( val % 16 )]
+
+def mph2beaufort(miles):
+    kmh = m2k(miles)
+    x = 12
+    scale = [1, 5, 11, 19, 28, 38, 49, 61, 74, 88, 102, 117]
+    for beaufort, speed in enumerate(scale):
+        upper_limit = speed
+        if beaufort == 0:
+            lower = 0
+        else:
+            lower = scale[beaufort - 1]
+        print(lower, upper))
+        if (kmh >= lower) and (kmh < upper):
+            x = beaufort
+            break
+    return x
 
 if __name__ == '__main__':
-    utc = "2020-08-15 13:50:00"
-    print(utc2local(utc))
+    
+    #for v in [1.2, 8.1, 4.6, 32.2, 64.4]:
+    #    print("%s: %s"  % (v, mph2beaufort(v)))
+    v = 12
+    print("%s: %s"  % (v, mph2beaufort(v)))
+
