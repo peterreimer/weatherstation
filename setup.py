0077e76d14c6621fad75caf3d49e944187a84786
Peter Reimer <peter@4pi.org>
2020-08-17 13:26:59 +0200

    dist/package relevant files

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='weatherstation',
+    version='0.0.2',
+	author='Peter Reimer',
+    author_email='peter@4pi.org',
+    packages=find_packages(),
+	include_package_data=True,
+    zip_safe=False,
+    install_requires=[
+        'flask',
+    ],
+)
